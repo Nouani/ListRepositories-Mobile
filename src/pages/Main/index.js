e3278093910993026622112b584d99dae66fba13
Nouani <nouanigabriel@hotmail.com>
2020-02-09 17:30:43 -0300

    Estilização do form

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,34 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import {} from 'react-native';
 
-import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-export default function Main() {
-    return <Container />;
+import { Container, Form, Input, SubmitButton } from './styles';
+
+export default class Main extends Component {
+    state = {
+        newUser: '',
+        users: [],
+    };
+
+    render() {
+        const { users, newUser } = this.state;
+
+        return (
+            <Container>
+                <Form>
+                    <Input
+                        autoCorrect={false}
+                        autoCapitalize="none"
+                        placeholder="Adicionar usuário"
+                        value={newUser}
+                        onChangeText={text => this.setState({ newUser: text })}
+                    />
+                    <SubmitButton>
+                        <Icon name="add" size={29} color="#FFF" />
+                    </SubmitButton>
+                </Form>
+            </Container>
+        );
+    }
 }
